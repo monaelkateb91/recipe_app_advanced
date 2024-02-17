b0e2f6959ebadbc636f63a67f8322af154e6d45a
monaelkateb <mona.elkate91@gmail.com>
2024-02-17 03:04:54 +0200

    added changes

--- lib/pages/filter_page.dart
+++ lib/pages/filter_page.dart
@@ -26,82 +26,87 @@ class _FilterPageState extends State<FilterPage> {
     return Scaffold(
       appBar: AppBar(),
       body:
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
-                child: Wrap(
-                  // space between chips
-                    spacing: 10,
-                    // list of chips
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          selectedUserValue['type'] = "breakfast";
-                          setState(() {});
-                        },
-                        child: Chip(
-                          label: Text('Breakfast'),
-                          backgroundColor: selectedUserValue['type'] == "breakfast"
-                              ? Colors.orange
-                              : Colors.grey,
-                          padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                        ),
-                      ),
-                      InkWell(
-                        onTap: () {
-                          selectedUserValue['type'] = "lunch";
-                          setState(() {});
-                        },
-                        child: Chip(
-                          label: Text('Lunch'),
-                          backgroundColor: selectedUserValue['type'] == "lunch"
-                              ? Colors.orange
-                              : Colors.grey,
-                          padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                        ),
-                      ),
-                      InkWell(
-                        onTap: () {
-                          selectedUserValue['type'] = "dinner";
-                          setState(() {});
-                        },
-                        child: Chip(
-                          label: Text('Dinner'),
-                          backgroundColor: selectedUserValue['type'] == "dinner"
-                              ? Colors.orange
-                              : Colors.grey,
-                          padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                        ),
-                      ),
-                    ]),
-              ),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [GestureDetector(onTap: (){Navigator.of(context).pop();},
+            child:Container(height: 60,child: Text('clear'),)),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
+                    child: Wrap(
+                      // space between chips
+                        spacing: 10,
+                        // list of chips
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              selectedUserValue['type'] = "breakfast";
+                              setState(() {});
+                            },
+                            child: Chip(
+                              label: Text('Breakfast'),
+                              backgroundColor: selectedUserValue['type'] == "breakfast"
+                                  ? Colors.orange
+                                  : Colors.grey,
+                              padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {
+                              selectedUserValue['type'] = "lunch";
+                              setState(() {});
+                            },
+                            child: Chip(
+                              label: Text('Lunch'),
+                              backgroundColor: selectedUserValue['type'] == "lunch"
+                                  ? Colors.orange
+                                  : Colors.grey,
+                              padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {
+                              selectedUserValue['type'] = "dinner";
+                              setState(() {});
+                            },
+                            child: Chip(
+                              label: Text('Dinner'),
+                              backgroundColor: selectedUserValue['type'] == "dinner"
+                                  ? Colors.orange
+                                  : Colors.grey,
+                              padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                            ),
+                          ),
+                        ]),
+                  ),
     Padding(
     padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),),
-            Slider(min: 1, max:200
-              ,  value: selectedUserValue['servings'], onChanged: (double newvalue){
-                  selectedUserValue['servings']=newvalue.round();
-                  setState(() {
+                Slider(min: 1, max:200
+                  ,  value: selectedUserValue['servings'], onChanged: (double newvalue){
+                      selectedUserValue['servings']=newvalue.round();
+                      setState(() {
 
-                  });
-                }), Padding(
-                padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),),
-              Slider(min: 1, max:200
-                  ,  value: selectedUserValue['total_time'], onChanged: (double newvalue){
-                    selectedUserValue['total_time']=newvalue.round();
-                    setState(() {
+                      });
+                    }), Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),),
+                  Slider(min: 1, max:200
+                      ,  value: selectedUserValue['total_time'], onChanged: (double newvalue){
+                        selectedUserValue['total_time']=newvalue.round();
+                        setState(() {
 
-                    });
-                  }), Padding(
-                padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),),
-              Slider(min: 1, max:200
-                  ,  value: selectedUserValue['calories'], onChanged: (double newvalue){
-                    selectedUserValue['calories']=newvalue.round();
-                    setState(() {
+                        });
+                      }), Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),),
+                  Slider(min: 1, max:200
+                      ,  value: selectedUserValue['calories'], onChanged: (double newvalue){
+                        selectedUserValue['calories']=newvalue.round();
+                        setState(() {
 
-                    });
-                  })],
+                        });
+                      })],
 
+              ),
+            ],
           ),
 
     );
